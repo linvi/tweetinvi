63c8fad999ae1429143755a113751101640937d8
linvi <imart.thomas@hotmail.com>
2015-10-01 00:17:23 +0100

    Issue #28 * Added documentation to RateLimit static class

--- Tweetinvi/RateLimit.cs
+++ Tweetinvi/RateLimit.cs
@@ -5,7 +5,6 @@ using Tweetinvi.Core.Events.EventArguments;
 using Tweetinvi.Core.Interfaces.Controllers;
 using Tweetinvi.Core.Interfaces.Credentials;
 using Tweetinvi.Core.Interfaces.RateLimit;
-using Tweetinvi.Core.Interfaces.WebLogic;
 
 namespace Tweetinvi
 {
@@ -44,53 +43,83 @@ namespace Tweetinvi
             _helpController = TweetinviContainer.Resolve<IHelpController>();
         }
 
+        /// <summary>
+        /// Notify that a query is awaiting for RateLimits to become available in order to continue
+        /// </summary>
         public static event EventHandler<QueryAwaitingEventArgs> QueryAwaitingForRateLimit
         {
             add { _rateLimitAwaiter.QueryAwaitingForRateLimit += value; }
             remove { _rateLimitAwaiter.QueryAwaitingForRateLimit -= value; }
         }
 
+        /// <summary>
+        /// Configure how to Tweetinvi will handle RateLimits
+        /// </summary>
         public static RateLimitTrackerOptions RateLimitTrackerOption
         {
             get { return TweetinviConfig.CURRENT_RATELIMIT_TRACKER_OPTION; }
             set { TweetinviConfig.CURRENT_RATELIMIT_TRACKER_OPTION = value; }
         }
 
+        /// <summary>
+        /// Clear all the RateLimits information stored in the cache
+        /// </summary>
         public static void ClearRateLimitCache()
         {
             _rateLimitCache.ClearAll();
         }
 
+        /// <summary>
+        /// Clear a specific set of credentials RateLimits information stored in the cache
+        /// </summary>
         public static void ClearRateLimitCache(ITwitterCredentials credentials)
         {
             _rateLimitCache.Clear(credentials);
         }
 
+        /// <summary>
+        /// Wait for the rate limits to be available. This should be used before executing a query
+        /// </summary>
         public static void AwaitForQueryRateLimit(string query)
         {
             AwaitForQueryRateLimit(query, Auth.Credentials);
         }
 
+        /// <summary>
+        /// Wait for the rate limits to be available. This should be used before executing a query
+        /// </summary>
         public static void AwaitForQueryRateLimit(string query, ITwitterCredentials credentials)
         {
             _rateLimitAwaiter.WaitForCredentialsRateLimit(query, credentials);
         }
 
+        /// <summary>
+        /// Wait for the rate limits to be available. This should be used before executing a query
+        /// </summary>
         public static void AwaitForQueryRateLimit(ITokenRateLimit tokenRateLimit)
         {
             _rateLimitAwaiter.WaitForCredentialsRateLimit(tokenRateLimit);
         }
 
+        /// <summary>
+        /// Get the rate limits information for an url
+        /// </summary>
         public static ITokenRateLimit GetQueryRateLimit(string query)
         {
             return _rateLimitCacheManager.GetQueryRateLimit(query, Auth.Credentials);
         }
 
+        /// <summary>
+        /// Get the rate limits information for an url
+        /// </summary>
         public static ITokenRateLimit GetQueryRateLimit(string query, ITwitterCredentials credentials)
         {
             return _rateLimitCacheManager.GetQueryRateLimit(query, credentials);
         }
 
+        /// <summary>
+        /// Get all the rate limits of all the Twitter endpoints
+        /// </summary>
         public static ITokenRateLimits GetCurrentCredentialsRateLimits(bool useRateLimitCache = false)
         {
             ITokenRateLimits tokenRateLimits = null;
@@ -107,6 +136,9 @@ namespace Tweetinvi
             return tokenRateLimits;
         }
 
+        /// <summary>
+        /// Get all the rate limits of all the Twitter endpoints
+        /// </summary>
         public static ITokenRateLimits GetCredentialsRateLimits(ITwitterCredentials credentials, bool useRateLimitCache = false)
         {
             if (useRateLimitCache)
