44317c08027015d5ee62ace4ac30641771271ef4
linvi <imart.thomas@hotmail.com>
2015-10-06 01:03:27 +0100

    Issue #28 * Added documentation to search and stream static classes

--- Tweetinvi/Search.cs
+++ Tweetinvi/Search.cs
@@ -57,41 +57,65 @@ namespace Tweetinvi
             return SearchController.SearchTweets(tweetSearchParameters);
         }
 
+        /// <summary>
+        /// Search tweets with some additional metadata information
+        /// </summary>
         public static ISearchResult SearchTweetsWithMetadata(string searchQuery)
         {
             return SearchController.SearchTweetsWithMetadata(searchQuery);
         }
 
+        /// <summary>
+        /// Search tweets with some additional metadata information
+        /// </summary>
         public static ISearchResult SearchTweetsWithMetadata(ITweetSearchParameters tweetSearchParameters)
         {
             return SearchController.SearchTweetsWithMetadata(tweetSearchParameters);
         }
 
+        /// <summary>
+        /// Search direct replies to a specific tweet
+        /// </summary>
         public static IEnumerable<ITweet> SearchDirectRepliesTo(ITweet tweet)
         {
             return SearchController.SearchDirectRepliesTo(tweet);
         }
 
+        /// <summary>
+        /// Search replies to a tweet and recursively to the replies' replies
+        /// </summary>
         public static IEnumerable<ITweet> SearchRepliesTo(ITweet tweet, bool recursiveReplies)
         {
             return SearchController.SearchRepliesTo(tweet, recursiveReplies);
         }
 
+        /// <summary>
+        /// Create a parameter to search tweets for a specific query
+        /// </summary>
         public static ITweetSearchParameters CreateTweetSearchParameter(string query)
         {
             return SearchQueryParameterGenerator.CreateSearchTweetParameter(query);
         }
 
+        /// <summary>
+        /// Create a parameter to search tweets for a specific GeoCode
+        /// </summary>
         public static ITweetSearchParameters CreateTweetSearchParameter(IGeoCode geoCode)
         {
             return SearchQueryParameterGenerator.CreateSearchTweetParameter(geoCode);
         }
 
+        /// <summary>
+        /// Create a parameter to search tweets for some specific coordinates and radius
+        /// </summary>
         public static ITweetSearchParameters CreateTweetSearchParameter(ICoordinates coordinates, int radius, DistanceMeasure measure)
         {
             return SearchQueryParameterGenerator.CreateSearchTweetParameter(coordinates, radius, measure);
         }
 
+        /// <summary>
+        /// Create a parameter to search tweets for some specific coordinates and radius
+        /// </summary>
         public static ITweetSearchParameters CreateTweetSearchParameter(double longitude, double latitude, int radius, DistanceMeasure measure)
         {
             return SearchQueryParameterGenerator.CreateSearchTweetParameter(longitude, latitude, radius, measure);   
@@ -99,11 +123,17 @@ namespace Tweetinvi
 
         // USER
 
+        /// <summary>
+        /// Create a parameter to search users from a query
+        /// </summary>
         public static IUserSearchParameters CreateUserSearchParameter(string query)
         {
             return SearchQueryParameterGenerator.CreateUserSearchParameters(query);
         }
 
+        /// <summary>
+        /// Create a parameter to search users from a query at a specific page of the result indexation
+        /// </summary>
         public static IEnumerable<IUser> SearchUsers(string query, int maximumNumberOfResults = 20, int page = 0)
         {
             var searchParameters = CreateUserSearchParameter(query);
--- Tweetinvi/Stream.cs
+++ Tweetinvi/Stream.cs
@@ -20,26 +20,44 @@ namespace Tweetinvi
             _filteredStreamUnityFactory = TweetinviContainer.Resolve<IFactory<IFilteredStream>>();
         }
 
+        /// <summary>
+        /// Create a stream that receive tweets
+        /// </summary>
         public static ITweetStream CreateTweetStream()
         {
             return _tweetStreamUnityFactory.Create();
         }
 
+        /// <summary>
+        /// Create a stream that receive tweets. In addition this stream allow you to filter the results received.
+        /// </summary>
         public static ITrackedStream CreateTrackedStream()
         {
             return _trackedStreamUnityFactory.Create();
         }
 
+        /// <summary>
+        /// Create a a stream that get the tweets from the Twitter public Sample stream
+        /// https://dev.twitter.com/streaming/reference/get/statuses/sample
+        /// </summary>
         public static ISampleStream CreateSampleStream()
         {
             return _sampleStreamUnityFactory.Create();
         }
 
+        /// <summary>
+        /// Create a a stream that get the tweets from the Twitter public Sample stream
+        /// https://dev.twitter.com/streaming/reference/post/statuses/filter
+        /// </summary>
         public static IFilteredStream CreateFilteredStream()
         {
             return _filteredStreamUnityFactory.Create();
         }
 
+        /// <summary>
+        /// Create a a stream that get the tweets from the Twitter public Sample stream
+        /// https://dev.twitter.com/streaming/reference/get/user
+        /// </summary>
         public static IUserStream CreateUserStream()
         {
             return _userStreamFactory.Create();
