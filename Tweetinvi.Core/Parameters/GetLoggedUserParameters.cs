29e738bf00d0657f2416b7a66b10e2d9f7797a42
Thomas Imart <Thomas.Imart@orbussoftware.com>
2016-01-18 21:19:31 +0000

    - Add GetLoggedUser

---
+++ Tweetinvi.Core/Parameters/GetLoggedUserParameters.cs
@@ -0,0 +1,25 @@
+﻿using Tweetinvi.Core.Credentials;
+
+namespace Tweetinvi.Core.Parameters
+{
+    public interface IGetLoggedUserParameters : ICustomRequestParameters
+    {
+        bool IncludeEntities { get; set; }
+        bool IncludeEmail { get; set; }
+        bool SkipStatus { get; set; }
+    }
+
+    public class GetLoggedUserParameters : CustomRequestParameters, IGetLoggedUserParameters
+    {
+        public GetLoggedUserParameters()
+        {
+            IncludeEntities = true;
+            IncludeEmail = true;
+            SkipStatus = true;
+        }
+
+        public bool IncludeEntities { get; set; }
+        public bool IncludeEmail { get; set; }
+        public bool SkipStatus { get; set; }
+    }
+}
