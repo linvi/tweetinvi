438722e82d44a65e0523bdb90aa36041d4cadade
linvi <username@users.noreply.github.com>
2016-02-19 20:20:21 +0000

    Issue #50 - Added documentation on few classes

--- Tweetinvi.Core/Interfaces/Models/IWoeIdLocation.cs
+++ Tweetinvi.Core/Interfaces/Models/IWoeIdLocation.cs
@@ -1,8 +1,18 @@
 ﻿namespace Tweetinvi.Core.Interfaces.Models
 {
+    /// <summary>
+    /// Where On Earth ID : https://developer.yahoo.com/geo/geoplanet/guide/concepts.html
+    /// </summary>
     public interface IWoeIdLocation
     {
+        /// <summary>
+        /// Location name
+        /// </summary>
         string Name { get; set; }
+
+        /// <summary>
+        /// Location WOEID
+        /// </summary>
         long WoeId { get; set; }
     }
 }
