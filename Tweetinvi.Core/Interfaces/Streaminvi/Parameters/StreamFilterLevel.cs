eeeb1e9c718f94db2e3de7867b657ce659fc5ffe
linvi <linvi@users.noreply.github.com>
2016-02-25 14:53:05 +0000

    Issue #50 - StreamLevelFilter documentation

--- Tweetinvi.Core/Interfaces/Streaminvi/Parameters/StreamFilterLevel.cs
+++ Tweetinvi.Core/Interfaces/Streaminvi/Parameters/StreamFilterLevel.cs
@@ -1,5 +1,10 @@
 ﻿namespace Tweetinvi.Core.Interfaces.Streaminvi.Parameters
 {
+    /// <summary>
+    /// Level you want to use to filter tweets containing violence, sex or any sensible subjects.
+    /// None meaning that you accept ALL the tweets.
+    /// https://dev.twitter.com/streaming/overview/request-parameters#filter_level
+    /// </summary>
     public enum StreamFilterLevel
     {
         None,
