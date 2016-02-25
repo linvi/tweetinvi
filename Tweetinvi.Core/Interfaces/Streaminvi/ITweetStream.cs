9edebd235de5517965cf3de98008ffabeb72cf64
linvi <linvi@users.noreply.github.com>
2016-02-25 14:50:21 +0000

    Issue #50 - Added documentation to all of the stream interfaces

--- Tweetinvi.Core/Interfaces/Streaminvi/ITweetStream.cs
+++ Tweetinvi.Core/Interfaces/Streaminvi/ITweetStream.cs
@@ -6,7 +6,14 @@ namespace Tweetinvi.Core.Interfaces.Streaminvi
 {
     public interface ITweetStream : ITwitterStream
     {
+        /// <summary>
+        /// Event informing that a tweet has been received.
+        /// </summary>
         event EventHandler<TweetReceivedEventArgs> TweetReceived;
+
+        /// <summary>
+        /// Start a stream SYNCHRONOUSLY. The thread will continue after the stream has stopped.
+        /// </summary>
         Task StartStream(string url);
     }
 }
