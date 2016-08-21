ba579e422265828036431edd570295d7522ef941
Haseeb <Haseeb1431@gmail.com>
2016-08-21 23:31:35 +0500

    Pending core changes
    #311

--- Tweetinvi.Core/Core/Extensions/TweetSearchFiltersExtension.NetCore.cs
+++ Tweetinvi.Core/Core/Extensions/TweetSearchFiltersExtension.NetCore.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Tweetinvi.Core.Attributes;
 using Tweetinvi.Parameters;
+using System.Reflection;
 
 namespace Tweetinvi.Core.Extensions
 {
@@ -9,7 +10,7 @@ namespace Tweetinvi.Core.Extensions
         public static string GetQueryFilterName(this TweetSearchFilters tweetSearchFilters)
         {
             var field = tweetSearchFilters.GetType().GetField(tweetSearchFilters.ToString());
-            var descriptionAttribute = (TweetSearchFilterAttribute)Attribute.GetCustomAttribute(field, typeof(TweetSearchFilterAttribute));
+            var descriptionAttribute = (TweetSearchFilterAttribute)CustomAttributeExtensions.GetCustomAttribute(field, typeof(TweetSearchFilterAttribute));
             return descriptionAttribute != null ? descriptionAttribute.FilterName : tweetSearchFilters.ToString().ToLowerInvariant();
         }
     }
