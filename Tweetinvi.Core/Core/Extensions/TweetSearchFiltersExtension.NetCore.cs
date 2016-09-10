1969dd7e9b744bdb93a1c81adf7679bd4a4bcdce
Haseeb <Haseeb1431@gmail.com>
2016-09-11 01:25:53 +0500

    Added duplicate files with .netcore appended to their names. .NetStandard solutions will use these files directly
    There are few newer files that have non supported changes being done recently. We will review them and fixed in the the next batch
    #283

---
+++ Tweetinvi.Core/Core/Extensions/TweetSearchFiltersExtension.NetCore.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Tweetinvi.Core.Attributes;
+using Tweetinvi.Parameters;
+using System.Reflection;
+
+namespace Tweetinvi.Core.Extensions
+{
+    public static class TweetSearchFiltersExtension
+    {
+        public static string GetQueryFilterName(this TweetSearchFilters tweetSearchFilters)
+        {
+            var field = tweetSearchFilters.GetType().GetField(tweetSearchFilters.ToString());
+            var descriptionAttribute = (TweetSearchFilterAttribute)CustomAttributeExtensions.GetCustomAttribute(field, typeof(TweetSearchFilterAttribute));
+            return descriptionAttribute != null ? descriptionAttribute.FilterName : tweetSearchFilters.ToString().ToLowerInvariant();
+        }
+    }
+}
