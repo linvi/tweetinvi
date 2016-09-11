b3ca74bdbffffa71005e227d7793e9b9aa0dd545
Haseeb <Haseeb1431@gmail.com>
2016-09-11 23:19:57 +0500

    Switched to conditional code instead of separate file. completes #311 and #283
    Removed duplicate files and added conditional code. Once verified
    changes for both the solutions we can merge both the branches togather.

--- Tweetinvi.Core/Core/Events/WeakDelegate.cs
+++ Tweetinvi.Core/Core/Events/WeakDelegate.cs
@@ -15,7 +15,12 @@ namespace Tweetinvi.Core.Events
                 _targetReference = new WeakReference(realDelegate.Target);
             else
                 _targetReference = null;
+#if NET_CORE
+            _method = realDelegate.GetMethodInfo();
+#else
             _method = realDelegate.Method;
+#endif
+
         }
 
         public TDelegate GetDelegate()
@@ -25,6 +30,16 @@ namespace Tweetinvi.Core.Events
 
         private Delegate GetDelegateInternal()
         {
+#if NET_CORE
+            if (_targetReference != null)
+            {
+                return _method.CreateDelegate(typeof(TDelegate), _targetReference.Target);
+            }
+            else
+            {
+                return _method.CreateDelegate(typeof(TDelegate));
+            }
+#else
             if (_targetReference != null)
             {
                 return Delegate.CreateDelegate(typeof(TDelegate), _targetReference.Target, _method);
@@ -33,6 +48,7 @@ namespace Tweetinvi.Core.Events
             {
                 return Delegate.CreateDelegate(typeof(TDelegate), _method);
             }
+#endif
         }
 
         public bool IsAlive
@@ -47,7 +63,11 @@ namespace Tweetinvi.Core.Events
             Delegate d = (Delegate)(object)other;
             return d != null
                 && d.Target == _targetReference.Target
+#if NET_CORE
+                && d.GetMethodInfo().Equals(_method);
+#else
                 && d.Method.Equals(_method);
+#endif
         }
 
         #endregion
