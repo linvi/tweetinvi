fa64b25fdf7cb9cf1ffc805ebb3291e2cc767105
Max Wilkinson <mawilkin@umich.edu>
2016-10-30 13:31:09 -0400

    Added NETCore Example Project
    This project uses the NetStandard Tweetinvi libraries to showcase a
    .NETCore application making use of Tweetinvi.

---
+++ NetStandard/Examplinvi.NETCore/APICredentials.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Examplinvi.NETCore
+{
+    /// <summary>
+    /// Provides access to Twitter API credentials
+    /// </summary>
+    public class APICredentials
+    {
+        public string ConsumerKey { get; private set; }
+        public string ConsumerSecret { get; private set; }
+        public string AccessToken { get; private set; }
+        public string AccessTokenSecret { get; private set; }
+
+        public APICredentials()
+        {
+            GetAPICredentials();
+        }
+
+        /// <summary>
+        /// Uses .NET Core configuration API for extracting credentials from environment variables.
+        /// </summary>
+        private void GetAPICredentials()
+        {
+            var builder = new ConfigurationBuilder();
+            builder.AddEnvironmentVariables();
+
+            var configuration = builder.Build();
+
+            ConsumerKey = configuration["ConsumerKey"];
+            ConsumerSecret = configuration["ConsumerSecret"];
+            AccessToken = configuration["AccessToken"];
+            AccessTokenSecret = configuration["AccessTokenSecret"];
+        }
+    }
+}
---
+++ NetStandard/Examplinvi.NETCore/Examples/TimelineClient.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Tweetinvi;
+using Tweetinvi.Models;
+
+namespace Examplinvi.NETCore.Examples
+{
+    interface ITimelineClient
+    {
+        IEnumerable<ITweet> GetHomeTimeline();
+        IEnumerable<IMention> GetMentionsTimeline();
+        IEnumerable<ITweet> GetUserTimeline(string username);
+    }
+
+    public class TimelineClient : ITimelineClient
+    {
+        public IEnumerable<ITweet> GetHomeTimeline()
+        {
+            var user = User.GetAuthenticatedUser();
+            return user.GetHomeTimeline();
+        }
+
+        public IEnumerable<IMention> GetMentionsTimeline()
+        {
+            var user = User.GetAuthenticatedUser();
+            return user.GetMentionsTimeline();
+        }
+
+        public IEnumerable<ITweet> GetUserTimeline(string username)
+        {
+            var user = User.GetUserFromScreenName(username);
+            return user.GetUserTimeline();
+        }
+    }
+}
