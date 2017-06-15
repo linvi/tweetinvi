2331f39a811ca5bf2ed987990630424a16919d36
linvi <linvi@users.noreply.github.com>
2017-06-15 14:32:48 +0100

    Updated .NETStandard target back to 1.3

--- Examplinvi.ASP.NET/MyCredentials.cs
+++ Examplinvi.ASP.NET/MyCredentials.cs
@@ -4,10 +4,10 @@ namespace Examplinvi.ASP.NET
 {
     public static class MyCredentials
     {
-        public static string CONSUMER_KEY = "54aUlLwbmRN7SX3wswMJmqpQG";
-        public static string CONSUMER_SECRET = "Fid46uknw6665bA4NxCiOHPoIUv6KMsaxhul7P4MwHMecIytYw";
-        public static string ACCESS_TOKEN = "1693649419-Ubxt4bKWWGQiRY9Ko4BcvX03EJUm2BPcRbW6pPM";
-        public static string ACCESS_TOKEN_SECRET = "wLa7UFyp4FEDR2MHPtr6SEy4E3iCBqqlNAXuampl2SXZ7";
+        public static string CONSUMER_KEY = "CONSUMER_KEY";
+        public static string CONSUMER_SECRET = "CONSUMER_SECRET";
+        public static string ACCESS_TOKEN = "ACCESS_TOKEN";
+        public static string ACCESS_TOKEN_SECRET = "ACCESS_TOKEN_SECRET";
 
         public static ITwitterCredentials GenerateCredentials()
         {
