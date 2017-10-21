0839c20544f25774efcebf9af18ee5079d2f459b
linvi <linvi@users.noreply.github.com>
2017-10-21 16:55:45 -0700

    #95 Removed DataContract / EnumMember to remove the need of System.Runtime.Serialization.Primitives

--- Tweetinvi.Core/Public/Models/Enum/HttpMethod.cs
+++ Tweetinvi.Core/Public/Models/Enum/HttpMethod.cs
@@ -5,16 +5,11 @@ namespace Tweetinvi.Models
     /// <summary>
     /// Enumeration of possible HTTP request method
     /// </summary>
-    [DataContract]
     public enum HttpMethod
     {
-        [EnumMember]
         GET,
-        [EnumMember]
         POST,
-        [EnumMember]
         PUT,
-        [EnumMember]
         DELETE
     }
 }
--- Tweetinvi.WebLogic/Utils/OAuthConnectionType.cs
+++ Tweetinvi.WebLogic/Utils/OAuthConnectionType.cs
@@ -5,12 +5,9 @@ namespace Tweetinvi.WebLogic.Utils
     /// <summary>
     /// Provide different solution to connect to an url
     /// </summary>
-    [DataContract]
     public enum OAuthConnectionType
     {
-        [EnumMember]
         AuthorizationHeaders,
-        [EnumMember]
         BaseHtmlString
     }
 }
