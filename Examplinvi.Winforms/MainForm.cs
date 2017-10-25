66415041ebc3caa019f26ff11faac0026ec84fac
linvi <linvi@users.noreply.github.com>
2017-10-25 15:47:19 -0700

    #95 Added Winform Example project

---
+++ Examplinvi.Winforms/MainForm.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+using System.Windows.Forms;
+using Tweetinvi;
+
+namespace Examplinvi.Winforms
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, System.EventArgs e)
+        {
+            Auth.SetUserCredentials("CONSUMER_KEY", "CONSUMER_SECRET", "ACCESS_TOKEN", "ACCESS_TOKEN_SECRET");
+
+            var user = User.GetAuthenticatedUser();
+
+            textBox.Text = string.Format("You are now authenticated as {0}!", user);
+        }
+    }
+}
