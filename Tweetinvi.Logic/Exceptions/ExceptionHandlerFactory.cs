fd0e4ef4580ab0bc14af2e8ee2df417817c33398
Josh Keegan <josh.keegan@gmx.com>
2018-12-14 12:04:23 +0000

    Update TODO with comment explaining the rather unusual lock-free design

--- Tweetinvi.Logic/Exceptions/ExceptionHandlerFactory.cs
+++ Tweetinvi.Logic/Exceptions/ExceptionHandlerFactory.cs
@@ -23,9 +23,23 @@ namespace Tweetinvi.Logic.Exceptions
         {
             if (_exceptionHandler.Value == null)
             {
-                // TODO: Lock on creation??
-                // Theoretically, we could be making the exception handler at two times within the same async flow on two different threads...
-                // Also check what happens here as the pointer will change & it may be that the pointer doesn't propagate back upwards.
+                // Note: it is safe to set _exceptionHandler.Value without locking, as even though it could be accessed
+                //  concurrently from multiple places within an async flow, it can only be set in one.
+                //  This is because the execution context doesn't get copied back to the calling thread.
+                //
+                // Example:
+                //  Method A starts method B asynchronously without an ExceptionHandler in the execution context.
+                //  Method A doesn't await method B, so they both run concurrently.
+                //  Both call IExceptionHandlerFactory.Create()
+                //  Method B has a shallow copy of the execution context of method A, and method A didn't have
+                //      an ExceptionHandler, so neither method has an ExceptionHandler.
+                //  An ExceptionHandler is made for method A and stored in its Execution Context
+                //  An ExceptionHandler is made for method B and stored in its Execution Context
+                //  Despite being in the same async flow, they each get their own ExceptionHandler, so there's no locking
+                //  required.
+                //
+                // Note: the above example also demonstrates why external calls must use the methods in Sync
+                //  which would ensure an ExceptionHandler exists in A before spawning B, thus sharing them.
                 _exceptionHandler.Value = new ExceptionHandler(_twitterExceptionFactory);
             }
 
