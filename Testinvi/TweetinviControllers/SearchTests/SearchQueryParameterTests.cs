e64fcbf0659dfe8649bb1e56e8ebb5c3acca6ed0
Josh Keegan <josh.keegan@gmx.com>
2019-01-24 13:23:33 +0000

    fix #787 - Search text being encoded twice

---
+++ Testinvi/TweetinviControllers/SearchTests/SearchQueryParameterTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FakeItEasy;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tweetinvi.Controllers.Search;
+
+namespace Testinvi.TweetinviControllers.SearchTests
+{
+    [TestClass]
+    public class SearchQueryParameterTests
+    {
+        [TestMethod]
+        public void TestGenerateSearchQueryParameterStringDoesNotUrlEncode()
+        {
+            // Arrange
+            const string expected = "Something that will get URL encoded";
+            ISearchQueryValidator queryValidator = A.Fake<ISearchQueryValidator>();
+            SearchQueryParameterGenerator paramGenerator = new SearchQueryParameterGenerator(queryValidator);
+
+            // Act
+            string actual = paramGenerator.GenerateSearchQueryParameter(expected);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
