a42973912702c2ff2d4ea61905d3d1db05a72f46
linvi <linvi@users.noreply.github.com>
2020-03-01 08:24:18 +0000

    Updated EndToEndTestConfig for better support of upcoming tests

--- Tests/xUnitinvi/EndToEnd/EndToEndTestConfig.cs
+++ Tests/xUnitinvi/EndToEnd/EndToEndTestConfig.cs
@@ -6,29 +6,37 @@ namespace xUnitinvi.EndToEnd
     {
         public static readonly bool ShouldRunEndToEndTests = false;
         public static readonly bool ShouldRunRateLimitHungryTests = false;
+        public static readonly bool ShouldRunAccountActivityStreamTests = false; // very slow tests
 
         public static readonly IntegrationTestAccount TweetinviApi = new IntegrationTestAccount
         {
-            Credentials = new TwitterCredentials("", "", "",    ""),
-            AccountId = "tweetinviapi"
+            Credentials = new TwitterCredentials("", "", "", "")
+            {
+                BearerToken = ""
+            },
+            AccountId = "tweetinviapi",
+            UserId = 1577389800
         };
 
         public static readonly IntegrationTestAccount TweetinviTest = new IntegrationTestAccount
         {
-            Credentials = new TwitterCredentials("", "", "",    ""),
-            AccountId = "tweetinvitest"
+            Credentials = new TwitterCredentials("", "", "", ""),
+            AccountId = "tweetinvitest",
+            UserId = 1693649419
         };
 
         public static readonly IntegrationTestAccount ProtectedUser = new IntegrationTestAccount
         {
-            Credentials = new TwitterCredentials("", "", "",    ""),
-            AccountId = "artwolkt"
+            Credentials = new TwitterCredentials("", "", "", ""),
+            AccountId = "artwolkt",
+            UserId = 42290825
         };
 
         public static readonly IntegrationTestAccount ProtectedUserAuthenticatedToTweetinviApi = new IntegrationTestAccount
         {
             Credentials = new TwitterCredentials("", "", "", ""),
-            AccountId = "artwolkt"
+            AccountId = "artwolkt",
+            UserId = 42290825
         };
     }
 
@@ -36,6 +44,7 @@ namespace xUnitinvi.EndToEnd
     {
         public ITwitterCredentials Credentials { get; set; }
         public string AccountId { get; set; }
+        public long UserId { get; set; }
 
         public static implicit operator string (IntegrationTestAccount account)
         {
