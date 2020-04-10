208cd349b1c9ff58deec20647c3741466fbce4cf
linvi <linvi@users.noreply.github.com>
2020-04-10 21:29:04 +0100

    Added upload video end to end tests

---
+++ Tests/xUnitinvi/EndToEnd/UploadEndToEndTests.cs
@@ -0,0 +1,42 @@
+using System.Net.Http;
+using System.Threading.Tasks;
+using Tweetinvi.Parameters;
+using Xunit;
+using Xunit.Abstractions;
+using xUnitinvi.TestHelpers;
+
+namespace xUnitinvi.EndToEnd
+{
+    [Collection("EndToEndTests")]
+    public class UploadEndToEndTests : TweetinviTest
+    {
+        public UploadEndToEndTests(ITestOutputHelper logger) : base(logger)
+        {
+        }
+
+        [Fact]
+        public async Task UploadVideo()
+        {
+            if (!EndToEndTestConfig.ShouldRunEndToEndTests)
+                return;
+
+            var httpClient = new HttpClient();
+
+            _logger.WriteLine("Downloading video...");
+            var videoBinary = await httpClient.GetByteArrayAsync("https://github.com/linvi/tweetinvi.issues/raw/master/sample_video.mp4");
+            _logger.WriteLine("Video downloaded");
+
+            var uploadedVideo = await _tweetinviTestClient.Upload.UploadVideo(videoBinary);
+            await _tweetinviTestClient.Upload.WaitForMediaProcessingToGetAllMetadata(uploadedVideo);
+
+            var tweet = await _tweetinviTestClient.Tweets.PublishTweet(new PublishTweetParameters("superb video...")
+            {
+                Medias = {uploadedVideo}
+            });
+
+            await tweet.Destroy();
+
+            Assert.Equal(tweet.Media.Count, 1);
+        }
+    }
+}
