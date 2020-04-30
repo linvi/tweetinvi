317e582116ebef302d5605b52334b155a8d4aee4
linvi <linvi@users.noreply.github.com>
2020-04-30 11:35:22 +0100

    Added builder as a global script to make nuget process easier. Cleanup old builder

--- tools/tweetinvi-deploy-locally.py
+++ tools/tweetinvi-deploy-locally.py
@@ -4,6 +4,7 @@ import argparse
 import subprocess
 import sys
 import os
+import pathlib
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--install', action='store_true')
@@ -18,8 +19,9 @@ if args.install:
     print('bootstrap in progress...')
     os.popen('nuget sources add -Name tweetinvi -Source $(pwd)').read()
     os.popen('nuget sources add -Name tweetinvi -ConfigFile ~/.nuget/NuGet/NuGet.Config -Source ' + scriptDirectoryPath).read()
-    os.popen('cp ./deploy-locally.py /usr/local/bin').read()
-    rootPath = '/Users/linvi'
+    os.popen('cp ./tweetinvi-builder.py /usr/local/bin').read()
+    os.popen('cp ./tweetinvi-deploy-locally.py /usr/local/bin').read()
+    rootPath = str(pathlib.Path.home())
 
     if len(os.popen('cat ' + rootPath + '/.zshrc | grep TWEETINVI_BUILDER_PATH').read()) == 0:
         zshrc = open(rootPath + "/.zshrc", "a+")
@@ -37,21 +39,22 @@ if args.uninstall:
     print('uninstalling in progress...')
     os.popen('nuget sources remove -Name tweetinvi').read()
     os.popen('nuget sources remove -Name tweetinvi -ConfigFile ~/.nuget/NuGet/NuGet.Config').read()
-    os.popen('rm /usr/local/bin/deploy-locally.py').read()
+    os.popen('rm /usr/local/bin/tweetinvi-deploy-locally.py').read()
+    os.popen('rm /usr/local/bin/tweetinvi-builder.py').read()
     sys.exit(0)
 
 tweetinviNugetSourceInstalled = len(os.popen('nuget sources | grep tweetinvi').readlines()) == 0
 
 if tweetinviNugetSourceInstalled:
     print('You need to bootstrap your machine to run the build')
-    print('Please run ./deploy-locally.py --install')
+    print('Please run ./tweetinvi-deploy-locally.py --install')
     sys.exit(1)
 
-version = os.popen('$TWEETINVI_BUILDER_PATH/builder.py --build-version').read()
-print('builder.py currently targets version ' + version)
+version = os.popen('$TWEETINVI_BUILDER_PATH/tweetinvi-builder.py --build-version').read()
+print('tweetinvi-builder.py currently targets version ' + version)
 
 if args.add:
     subprocess.call('dotnet add package tweetinviapi -v ' + version, shell=True)
 
-if args.remove:
+if args.rm:
     subprocess.call('dotnet remove package tweetinviapi', shell=True)
